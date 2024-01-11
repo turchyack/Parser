765d83f240d5918da713d8d5bf78a491c9540a3a
turchyack <turchyack@gmail.com>
2024-01-12 02:25:27 +0300

    Добавил функцию read_value

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -17,51 +17,45 @@ public:
     uint16_t EndMarker;
 };
 
+void read_value(std::istream& stream, uint16_t& value) {
+    stream.read(reinterpret_cast<char*>(&value), sizeof(value));
+    std::cout << value << std::endl;
+   if (static_cast<size_t>(stream.gcount()) < sizeof(value)) {
+        return;
+    }
+}
+
+void read_file(const std::string& fpath) {
+    printf("debug: read_file(%s)\n", fpath.c_str());
+
+    auto file = std::ifstream(fpath, std::ios::binary);
+   
+    const size_t block_size = 100;
+    while (true) {
+        Frame frame;
+        
+        read_value(file, frame.Version);
+      
+    }
+}
+
 class FrameParser {
 private:
     std::vector<Frame> Frames;
 public:
     void LoadFromFile(const std::string& fileName) {
-
-       std::ifstream file(fileName, std::ios::binary);
-               
-        while (file.peek() != EOF) {
-            Frame frame;
-            file.read(reinterpret_cast<char*>(&frame.Version), sizeof(uint16_t));
-            char identifier[2];
-            file.read(identifier, 2);
-            frame.Identifier = std::string(identifier, 2);
-            file.seekg(22);
-            file.read(reinterpret_cast<char*>(&frame.Address), sizeof(uint16_t));
-
-            frame.Address = htons(frame.Address);
-
-            file.read(reinterpret_cast<char*>(&frame.Command), sizeof(uint16_t));
-
-            file.read(reinterpret_cast<char*>(&frame.DataLength), sizeof(uint32_t));
-   
-            frame.DataLength = htonl(frame.DataLength);
-
-            frame.Data.resize(frame.DataLength);
-
-            file.read(reinterpret_cast<char*>(&frame.Data[0]), frame.DataLength);
-          
-            file.read(reinterpret_cast<char*>(&frame.Checksum), sizeof(uint16_t));
-
-
-            file.read(reinterpret_cast<char*>(&frame.EndMarker), sizeof(uint16_t));
-
-            Frames.push_back(frame);
-            std::cout << i << " frame" << std::endl;
-        }
-
-        file.close();
+      // std::ifstream file(fileName, std::ios::binary);
+        read_file(fileName);
+       
     }
 
     void SaveToText(const std::string& txtFileName) {
     }
 
 };
+
+
+
 
 int main() {
     FrameParser frameParser;
