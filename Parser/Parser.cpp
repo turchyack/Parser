aef4e34472bddbdfa59cb45f09b7c72be388513e
turchyack <turchyack@gmail.com>
2024-01-12 06:28:16 +0300

    реализованы 3/4 перегрузок функции read_value

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -8,7 +8,7 @@
 class Frame {
 public:
     uint16_t Version;
-    std::string Identifier;
+    char Identifier[2];
     uint16_t Address;
     uint16_t Command;
     uint32_t DataLength;
@@ -19,11 +19,55 @@ public:
 
 void read_value(std::istream& stream, uint16_t& value) {
     stream.read(reinterpret_cast<char*>(&value), sizeof(value));
-    std::cout << value << std::endl;
-   if (static_cast<size_t>(stream.gcount()) < sizeof(value)) {
+    value = htons(value);
+    if (static_cast<size_t>(stream.gcount()) < sizeof(value)) {
         return;
     }
 }
+void read_value(std::istream& stream, uint32_t& value) {
+
+    stream.read(reinterpret_cast<char*>(&value), sizeof(value));  
+    value = htonl(value);
+    if (static_cast<size_t>(stream.gcount()) < sizeof(value)) {
+        return;
+    }
+}
+void read_value(std::istream& stream, std::vector<uint8_t>& value, size_t length) {
+    value.resize(length);
+    stream.read(reinterpret_cast<char*>(value.data()), length);
+    if (static_cast<size_t>(stream.gcount()) < length) {
+        return;
+    }
+}
+void skip_read(std::istream& stream, size_t value) {
+
+    stream.seekg(value, stream.cur);
+
+
+}
+void read_value(std::istream& stream, char& value) {
+
+    skip_read(stream, 2);
+   /* stream.read(&value, sizeof(value));
+    std::cout << sizeof(value) <<"DEBUG   gogoo  " << value << std::dec << std::endl;
+    
+    std::cout << value << std::dec << std::endl;
+   
+    if (static_cast<size_t>(stream.gcount()) < sizeof(value)) {
+        return;
+    }                                                           */                                // переделать на енумы пока не работает
+}
+
+
+
+
+
+
+
+
+
+
+
 
 void read_file(const std::string& fpath) {
     printf("debug: read_file(%s)\n", fpath.c_str());
@@ -33,8 +77,38 @@ void read_file(const std::string& fpath) {
     const size_t block_size = 100;
     while (true) {
         Frame frame;
-        
+
         read_value(file, frame.Version);
+        read_value(file, *frame.Identifier);
+        skip_read(file, 18);
+        read_value(file, frame.Address);
+        read_value(file, frame.Command);
+        read_value(file, frame.DataLength);
+        std::cout << frame.DataLength << std::endl;
+        read_value(file, frame.Data, frame.DataLength);
+        
+        read_value(file, frame.Checksum);
+        read_value(file, frame.EndMarker);
+        std::cout << file.tellg() << std::endl;
+        int a;
+        std::cin >> a;
+       
+
+       // read_value(file, frame.Version);
+      ////  read_value(file, *frame.Identifier);
+      //  read_value(file, frame.DataLength);
+      ////  frame.DataLength = 32;
+      //  read_value(file, frame.Data, frame.DataLength);
+
+
+
+
+       /* for (uint8_t elem : frame.Data) {
+            std::cout <<std::hex << elem << "-";
+        }
+        std::cout << std::hex << frame.Data.data() << std::endl;*/
+      //  read_value(file, frame.Data, frame.DataLength);
+               
       
     }
 }
