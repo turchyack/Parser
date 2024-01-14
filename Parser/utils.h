e90666bd8394f79625036e02dbce96cbffc27bd3
turchyack <turchyack@gmail.com>
2024-01-14 14:27:06 +0300

    utils.h

--- Parser/utils.h
+++ Parser/utils.h
@@ -2,11 +2,11 @@
 #include <fstream>
 #include <vector>
 namespace utils {
-	void ReadValue(std::istream& stream, uint16_t& value);
+void ReadValue(std::istream& stream, uint16_t& value);
 
-	void ReadValue(std::istream& stream, uint32_t& value);
+void ReadValue(std::istream& stream, uint32_t& value);
 
-	void ReadValue(std::istream& stream, std::vector<uint8_t>& value, size_t length);
+void ReadValue(std::istream& stream, std::vector<uint8_t>& value, size_t length);
 
-	void SkipRead(std::istream& stream, size_t length);
+void SkipRead(std::istream& stream, size_t length);
 }
